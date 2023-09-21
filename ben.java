8f367ccdab2eaf88dac5cb362b541825ff75b822
Isaac Ntim <isaac.ntim@gmail.com>
2023-09-20 22:57:33 -0400

    my test commit

---
+++ ben.java
@@ -0,0 +1,6 @@
+cho "Welcome to Zelle"                    = Isaac
+    echo "Zelle your payment gateway"          = Ben        
+    echo "Zelle is the gateway of the future"  = UnitedCore   
+    echo "Zelle is offering 30% discount"      = Emma
+    echo "Good deals for returning clients"     = Theo
+
